804f4ceada98104b7fbca00654c0ab4ca5be0707
amelia <josephine.suguna@gmail.com>
2021-07-17 23:24:36 -0700

    code

---
+++ amelia/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PostList from './PostList';
+import PostForm from './PostForm';
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm></PostForm>
+    </div>
+  );
+}
+
+export default App;
